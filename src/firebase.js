30823e541d8394e5829a4d155d770a63d71db6b8
Pratik-Nawale <pratiknawale03@gmail.com>
2023-03-19 08:09:05 +0530

    create built

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import * as dotenv from "dotenv";
 
-dotenv.config();
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-
 
   apiKey: process.env.FIREBASE_APIKEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
